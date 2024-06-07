3a27ebc98cad056a90a102cd207121165119eb0e
baker-1ee <ljw722007@gmail.com>
2024-06-07 16:29:42 +0900

    update notes

--- src/mysql/12.sql
+++ src/mysql/12.sql
@@ -12,8 +12,38 @@ select now(), sleep(2), now() from dual;
 select sysdate(), sleep(2), sysdate(), now() from dual;
 
 -- date_format
-select date_format(now(), '%Y-%m-%d') from dual;
-select date_format(now(), '%Y-%m-%d %H:%i:%s') from dual;
+SELECT DATE_FORMAT(NOW(), '%Y-%m-%d') FROM dual;
+SELECT DATE_FORMAT(NOW(), '%Y-%m-%d %H:%i:%s') FROM dual;
 -- str_to_date
-select str_to_date('2024-06-04', '%Y-%m-%d') from dual;
-select str_to_date('2024-06-04 15:00:00', '%Y-%m-%d %H:%i:%s') from dual;
+SELECT STR_TO_DATE('2024-06-04', '%Y-%m-%d') FROM dual;
+SELECT STR_TO_DATE('2024-06-04 15:00:00', '%Y-%m-%d %H:%i:%s') FROM dual;
+
+-- 날짜와 시간의 연산
+-- 날짜 더하기
+SELECT DATE_ADD(NOW(), INTERVAL 1 YEAR) AS next_year FROM dual;
+SELECT DATE_ADD(NOW(), INTERVAL 1 MONTH) AS next_month FROM dual;
+SELECT DATE_ADD(NOW(), INTERVAL 1 DAY) AS tomorrow FROM dual;
+SELECT DATE_ADD(NOW(), INTERVAL 1 HOUR) AS next_hour FROM dual;
+SELECT DATE_ADD(NOW(), INTERVAL 1 MINUTE) AS next_minute FROM dual;
+SELECT DATE_ADD(NOW(), INTERVAL 1 SECOND) AS next_second FROM dual;
+SELECT DATE_ADD(NOW(), INTERVAL 1 MICROSECOND) AS next_microsecond FROM dual;
+SELECT DATE_ADD(NOW(), INTERVAL 1 QUARTER) AS next_quarter FROM dual;
+SELECT DATE_ADD(NOW(), INTERVAL 1 WEEK) AS next_week FROM dual;
+-- 날짜 빼기
+SELECT DATE_ADD(NOW(), INTERVAL -1 YEAR) AS last_year FROM dual;
+SELECT DATE_ADD(NOW(), INTERVAL -1 MONTH) AS last_month FROM dual;
+SELECT DATE_ADD(NOW(), INTERVAL -1 DAY) AS yesterday FROM dual;
+SELECT DATE_ADD(NOW(), INTERVAL -1 HOUR) AS last_hour FROM dual;
+SELECT DATE_ADD(NOW(), INTERVAL -1 MINUTE) AS last_minute FROM dual;
+SELECT DATE_ADD(NOW(), INTERVAL -1 SECOND) AS last_second FROM dual;
+SELECT DATE_ADD(NOW(), INTERVAL -1 MICROSECOND) AS last_microsecond FROM dual;
+SELECT DATE_ADD(NOW(), INTERVAL -1 QUARTER) AS last_quarter FROM dual;
+SELECT DATE_ADD(NOW(), INTERVAL -1 WEEK) AS last_week FROM dual;
+
+select unix_timestamp() from dual;
+select unix_timestamp('2024-06-05 18:00:00') from dual;
+select from_unixtime(unix_timestamp('2024-06-05 18:00:00')) from dual;
+-- 2038
+select unix_timestamp('2038-01-19 03:14:08') from dual;
+select from_unixtime(unix_timestamp('2038-01-19 03:14:08')) from dual;
+select from_unixtime(1717577431.999999999999999999999999999998) from dual;
--- src/oracle/12.sql
+++ src/oracle/12.sql
@@ -12,4 +12,19 @@ select  sysdate, sysdate, systimestamp, systimestamp from dual;
 
 
 -- date
-select to_char(sysdate, 'YYYY-MM-DD') from dual;
+SELECT TO_CHAR(SYSDATE, 'YYYY-MM-DD') FROM dual;
+SELECT TO_CHAR(SYSDATE, 'YYYY-MM-DD HH24:MI:SS') FROM dual;
+
+SELECT TO_DATE('2024-06-04', 'YYYY-MM-DD') FROM dual;
+SELECT TO_DATE('2024-06-04 15:00:00', 'YYYY-MM-DD HH24:MI:SS') FROM dual;
+
+-- 날짜와 시간의 연산
+SELECT SYSDATE + 1 AS tomorrow FROM dual;
+SELECT ADD_MONTHS(SYSDATE, 1) AS next_month FROM dual;
+SELECT SYSDATE - 1 AS yesterday FROM dual;
+SELECT ADD_MONTHS(SYSDATE, -12) AS last_year FROM dual;
+SELECT TO_DATE('2024-06-04', 'YYYY-MM-DD') + 7 AS one_week_later FROM dual;
+SELECT ADD_MONTHS(TO_DATE('2024-06-04', 'YYYY-MM-DD'), -2) AS two_months_earlier FROM dual;
+
+SELECT (SYSDATE - TO_DATE('1970-01-01 00:00:00', 'YYYY-MM-DD HH24:MI:SS')) * 86400 AS current_timestamp FROM dual;
+select current_timestamp from dual;
